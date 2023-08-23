db876aa8facdb3f438b607da871f1a00ede91345
tiago <tiagoandroloko@gmail.com>
2023-08-23 19:36:45 -0300

    adicionando carrousel

---
+++ src/components/BoxSlide/BoxSlide.jsx
@@ -0,0 +1,19 @@
+import mars from "../../assets/img/planets/mars.png";
+import "./styles.scss";
+
+const BoxSlide = () => {
+  return (
+    <div className="box-slide">
+      <img src={mars} alt="Planeta do Sistema Solar" />
+
+      <p>
+        Marte é o quarto planeta a partir do Sol, o segundo menor do Sistema
+        Solar. Batizado em homenagem a divindade romana da guerra, muitas vezes
+        é descrito como o Planeta Vermelho, porque o óxido de ferro
+        predominante em sua superfície lhe dá uma aparência avermelhada.
+      </p>
+    </div>
+  );
+};
+
+export default BoxSlide;
--- src/components/Planets/Planets.jsx
+++ src/components/Planets/Planets.jsx
@@ -8,17 +8,15 @@ import "swiper/css/pagination";
 
 // import required modules
 import { Pagination } from "swiper/modules";
-
-import mars from '../../assets/img/planets/mars.png';
-import saturno from '../../assets/img/planets/saturno.webp';
-import jupiter from '../../assets/img/planets/jupiter.png';
+import BoxSlide from "../BoxSlide/BoxSlide";
 
 const Planets = () => {
   return (
-    <section className="planets-section">
+    <section className="planets-section container">
+      <h1 className="title">Planetas do Sistema Solar</h1>
 
-      {/* <Swiper
-        slidesPerView={3}
+      <Swiper
+        slidesPerView={"auto"}
         spaceBetween={30}
         pagination={{
           clickable: true,
@@ -26,14 +24,14 @@ const Planets = () => {
         modules={[Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide><img src={mars} width='400px' /></SwiperSlide>
-        <SwiperSlide><img src={saturno} width='400px' /></SwiperSlide>
-        <SwiperSlide><img src={jupiter} width='400px' /></SwiperSlide>
+        <SwiperSlide><BoxSlide /></SwiperSlide>
+        <SwiperSlide><BoxSlide /></SwiperSlide>
+        <SwiperSlide><BoxSlide /></SwiperSlide>
+        <SwiperSlide><BoxSlide /></SwiperSlide>
+        <SwiperSlide><BoxSlide /></SwiperSlide>
         
         
-      </Swiper> */}
-
-      <h1 className="title">Planetas do Sistema Solar</h1>
+      </Swiper>
 
     </section>
   );
