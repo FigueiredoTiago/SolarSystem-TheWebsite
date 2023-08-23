9f1e41b4ac6c50770cf02394a9706627faea7972
tiago <tiagoandroloko@gmail.com>
2023-08-23 09:15:18 -0300

    ajustes na home e section solar

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,24 +1,26 @@
 import "./styles.scss";
 import astronaut from '../../assets/img/astronaut.svg';
 import dog from '../../assets/img/dogNauta.svg';
-import earth from '../../assets/img/earth.svg';
+import earth from '../../assets/img/earth.svg';  
 
 const Home = () => {
   return (
-    <section className="home-section">
-      <section className="container home-container">
-
+    <section className="home-section container">
+      
         <img src={earth} alt="" className="earth" />
 
         <img src={dog} className="dog" />
 
-        <h1 className="home-title">Wellcome To Solar System</h1>
-        <h1 className="home-subtitle">Descubra os Segredos do Universo</h1>
+        <div className="info-title" >
 
-        <button className="button">Explore Now</button>
+          <h1 className="home-title">Wellcome To Solar System</h1>
+          <h1 className="home-subtitle">Descubra os Segredos do Universo</h1>
+          <button className="button">Explore Now</button>
+          
+        </div>
 
         <img src={astronaut} className="astronauta" />
-      </section>
+    
     </section>
   );
 };
